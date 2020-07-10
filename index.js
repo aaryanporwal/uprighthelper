e7dca40b352eae67e9eefabd9cb05c4638fa47f9
aaryanporwal <54525904+aaryanporwal@users.noreply.github.com>
2020-07-10 22:11:34 +0530

    Added all the files

---
+++ index.js
@@ -0,0 +1,72 @@
+const classifier = knnClassifier.create();
+const webcamElement = document.getElementById('webcam');
+let net;
+var audio = new Audio('audio_file.mp3');
+
+
+async function app() {
+  console.log('Loading mobilenet..');
+
+  // Load the model.
+  net = await mobilenet.load();
+  console.log('Sucessfully loaded model');
+
+  await setupWebcam();
+
+  // Reads an image from the webcam and associates it with a specific class
+  // index.
+  const addExample = classId => {
+    // Get the intermediate activation of MobileNet 'conv_preds' and pass that
+    // to the KNN classifier.
+    const activation = net.infer(webcamElement, 'conv_preds');
+
+    // Pass the intermediate activation to the classifier.
+    classifier.addExample(activation, classId);
+  };
+
+  // When clicking a button, add an example for that class.
+  document.getElementById('class-a').addEventListener('click', () => addExample(0));
+  document.getElementById('class-b').addEventListener('click', () => addExample(1));
+
+
+  while (true) {
+    if (classifier.getNumClasses() > 0) {
+      // Get the activation from mobilenet from the webcam.
+      const activation = net.infer(webcamElement, 'conv_preds');
+      // Get the most likely class and confidences from the classifier module.
+      const result = await classifier.predictClass(activation);
+
+      const classes = ['A', 'B'];
+      if(classes[result.classIndex]=="B"){
+        await audio.play();
+        document.body.style.backgroundColor = "red";
+      }
+      else{
+        document.body.style.backgroundColor = "white";
+      }
+    }
+
+    await tf.nextFrame();
+  }
+}
+
+async function setupWebcam() {
+  return new Promise((resolve, reject) => {
+    const navigatorAny = navigator;
+    navigator.getUserMedia = navigator.getUserMedia ||
+      navigatorAny.webkitGetUserMedia || navigatorAny.mozGetUserMedia ||
+      navigatorAny.msGetUserMedia;
+    if (navigator.getUserMedia) {
+      navigator.getUserMedia({ video: true },
+        stream => {
+          webcamElement.srcObject = stream;
+          webcamElement.addEventListener('loadeddata', () => resolve(), false);
+        },
+        error => reject());
+    } else {
+      reject();
+    }
+  });
+}
+
+app();
